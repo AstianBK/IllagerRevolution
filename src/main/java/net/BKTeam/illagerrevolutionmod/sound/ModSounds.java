2749961dd8c336d669b1c9c419e5266479c65510
SangreBK <94205579+SangreBK@users.noreply.github.com>
2023-04-02 23:27:21 -0300

    Added RUNE_TABLE_USE to ModSounds

--- src/main/java/net/BKTeam/illagerrevolutionmod/sound/ModSounds.java
+++ src/main/java/net/BKTeam/illagerrevolutionmod/sound/ModSounds.java
@@ -11,41 +11,44 @@ import net.BKTeam.illagerrevolutionmod.IllagerRevolutionMod;
 public class ModSounds {
         public static final DeferredRegister<SoundEvent> SOUND_EVENTS =
                 DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, IllagerRevolutionMod.MOD_ID);
+        
+        public static final RegistryObject<SoundEvent> RUNE_TABLE_USE =
+                registerSoundEvent("rune_table_use");
 
         public static final RegistryObject<SoundEvent> TAMER_WHISTLE =
                 registerSoundEvent("tamer_whistle");
 
-    public static final RegistryObject<SoundEvent> RAKER_HISS =
+        public static final RegistryObject<SoundEvent> RAKER_HISS =
             registerSoundEvent("raker_hiss");
 
-    public static final RegistryObject<SoundEvent> RAKER_MEOW =
+        public static final RegistryObject<SoundEvent> RAKER_MEOW =
             registerSoundEvent("raker_meow");
 
-    public static final RegistryObject<SoundEvent> SOUL_ABSORB =
+        public static final RegistryObject<SoundEvent> SOUL_ABSORB =
             registerSoundEvent("soul_absorb");
 
-    public static final RegistryObject<SoundEvent> SOUL_LIMIT =
+        public static final RegistryObject<SoundEvent> SOUL_LIMIT =
             registerSoundEvent("soul_limit");
 
-    public static final RegistryObject<SoundEvent> SOUL_RELEASE =
+        public static final RegistryObject<SoundEvent> SOUL_RELEASE =
             registerSoundEvent("soul_release");
 
-    public static final RegistryObject<SoundEvent> BLADE_KNIGHT_HURT =
+        public static final RegistryObject<SoundEvent> BLADE_KNIGHT_HURT =
             registerSoundEvent("blade_knight_hurt");
 
-    public static final RegistryObject<SoundEvent> BLADE_KNIGHT_SWORDHIT1 =
+        public static final RegistryObject<SoundEvent> BLADE_KNIGHT_SWORDHIT1 =
             registerSoundEvent("blade_knight_swordhit1");
 
-    public static final RegistryObject<SoundEvent> BLADE_KNIGHT_SWORDHIT2 =
+        public static final RegistryObject<SoundEvent> BLADE_KNIGHT_SWORDHIT2 =
             registerSoundEvent("blade_knight_swordhit2");
 
-    public static final RegistryObject<SoundEvent> BLADE_KNIGHT_LAUGH =
+        public static final RegistryObject<SoundEvent> BLADE_KNIGHT_LAUGH =
             registerSoundEvent("blade_knight_laugh");
 
-    public static final RegistryObject<SoundEvent> DEATH_MARK_SOUND =
+        public static final RegistryObject<SoundEvent> DEATH_MARK_SOUND =
             registerSoundEvent("death_mark_sound");
 
-    public static final RegistryObject<SoundEvent> BLEEDING_PROC =
+        public static final RegistryObject<SoundEvent> BLEEDING_PROC =
             registerSoundEvent("bleeding_proc");
 
 
