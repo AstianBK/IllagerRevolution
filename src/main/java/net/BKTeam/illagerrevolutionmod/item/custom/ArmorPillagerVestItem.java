672339384831f27896defa2b90d3c4e2dc432a59
SangreBK <94205579+SangreBK@users.noreply.github.com>
2023-04-08 07:03:07 -0300

    Add files via upload

---
+++ src/main/java/net/BKTeam/illagerrevolutionmod/item/custom/ArmorPillagerVestItem.java
@@ -0,0 +1,46 @@
+package net.BKTeam.illagerrevolutionmod.item.custom;
+
+import com.google.common.collect.ImmutableMap;
+import net.BKTeam.illagerrevolutionmod.item.ModArmorMaterials;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.ArmorMaterial;
+import software.bernie.geckolib3.core.IAnimatable;
+import software.bernie.geckolib3.core.PlayState;
+import software.bernie.geckolib3.core.controller.AnimationController;
+import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
+import software.bernie.geckolib3.core.manager.AnimationData;
+import software.bernie.geckolib3.core.manager.AnimationFactory;
+import software.bernie.geckolib3.item.GeoArmorItem;
+import software.bernie.geckolib3.util.GeckoLibUtil;
+
+import java.util.Map;
+
+public class ArmorPillagerVestItem extends GeoArmorItem implements IAnimatable {
+    private final AnimationFactory factory = GeckoLibUtil.createFactory(this);
+
+    private static final Map<ArmorMaterial, MobEffectInstance> MATERIAL_TO_EFFECT_MAP =
+            (new ImmutableMap.Builder<ArmorMaterial, MobEffectInstance>())
+                    .put(ModArmorMaterials.ILLAGERARMOR, new MobEffectInstance(MobEffects.UNLUCK, 0, 0)).build();
+
+    public ArmorPillagerVestItem(ModArmorMaterials material, EquipmentSlot slot, Properties settings) {
+        super(material, slot, settings);
+    }
+
+    @Override
+    public void registerControllers(AnimationData data) {
+        data.addAnimationController(new AnimationController<ArmorPillagerVestItem>(this, "controller",
+                20, this::predicate));
+    }
+
+    @Override
+    public AnimationFactory getFactory() {
+        return this.factory;
+    }
+
+    private <P extends IAnimatable> PlayState predicate(AnimationEvent<P> event) {
+        return PlayState.CONTINUE;
+    }
+
+}
